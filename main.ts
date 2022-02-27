94407b80a51496c66e41f9e397a72befd8c077e2
Fred Zuill <fzuill@users.noreply.github.com>
2022-02-27 15:10:15 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,16 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.clearScreen()
         count = receivedNumber % 25
         led.plot(count % 5, count / 5)
+        hasReceived = true
     }
 })
+let hasReceived = false
 let count = 0
 let isSender = false
 isSender = false
 count = 0
+hasReceived = false
+radio.setTransmitPower(7)
 radio.setGroup(1)
 radio.setFrequencyBand(0)
 radio.setTransmitPower(7)
@@ -22,4 +26,7 @@ loops.everyInterval(1000, function () {
         radio.sendNumber(count)
         count += 1
     }
+})
+loops.everyInterval(500, function () {
+	
 })
